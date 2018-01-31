dea90eb5939ef4230abf37f4520b556700d6ab4b
Klaudio Milankovic <klaudio_milankovic@bitbucket.org>
2018-01-31 14:55:36 +0100

    Form for adding recipes TODO: Send image to firebase storage

--- main.js
+++ main.js
@@ -15,39 +15,71 @@
 
   const database = firebase.database();
 
-  const dbData = firebase.database().ref().once('value').then(function(snapshot) {
+  const dbData = firebase.database().ref().once('value').then(function(snapshot) { // Retrieving values from DB
      let dbObject = snapshot.val();
   });
 
   const dbRef = database.ref();
 
- 
 
-  const recipes    = document.querySelector('#recipes');
-  const addRecipes = document.querySelector('#add-recipe');
+  // Form setup
+  const recipeForm          = document.querySelector('.add-recipes-form');
+  const submitButton        = document.querySelector('#add-recipe');
+  const addNewFields        = Array.from(document.querySelectorAll('.add-recipes-form__add-field'));
 
-  
-  // Adding recipes 
-  {// Needs to pickup ingretients from the form
-    const recept = {
-      ime : 'imetest',
-      sastojci : ['sastojak1','sastojak2'],
-      priprema : 'priprematest',
-      masnoca : 'niska'
-    }
-
-    function handleAddRecipes() {
-      dbRef.push().set(recept, function(error) {
-        if (error) {
-          console.log(`Data could not be saved. ${error}`)
-        } else {
-          console.log("Data successfully saved");
-        }
-      }); 
-    }
-
-    addRecipes.addEventListener('click', handleAddRecipes);
+  function handleNewFieldAdding(event) { // For adding multiple ingredients we duplicate inputs
+    let addToList = document.querySelector(`#${event.target.getAttribute(`data-add-new`)}`);
+    let listItem  = addToList.querySelector(`li:first-child`);
+    addToList.appendChild(listItem.cloneNode(true));
   }
 
-  
+  addNewFields.map((button) => {
+    button.addEventListener("click", handleNewFieldAdding);
+  });
+
+
+
+
+  function handleAddRecipes() {
+    let recipeName          = document.querySelector('#recipe-name').value;
+    let ingredients         = Array.from(document.querySelectorAll('#ingredients li'));
+    let cookingSteps        = Array.from(document.querySelectorAll('#cooking-steps li'));
+    let ingredientsData     = [];
+    let cookingStepsData    = [];
+
+    ingredients.filter(function(item) {
+      let ingredientName      = item.querySelector('[name="sastojak"]').value;
+      let ingredientQuantity  = item.querySelector('[name="kolicina"]').value;
+      // Extracting values from input and storing them into array
+      ingredientsData.push({ingredientName, ingredientQuantity});
+    });
+
+    cookingSteps.filter(function(item) {
+      let cookingStep      = item.querySelector('[name="korak-pripreme"]').value;
+      cookingStepsData.push(cookingStep);
+    });
+
+
+    let recipe = {
+      recipeName,
+      ingredientsData,
+      cookingStepsData,
+    }
+
+
+    dbRef.push().set(recipe, function(error) {
+      if (error) {
+        console.log(`Data could not be saved. ${error}`)
+      } else {
+        console.log("Data successfully saved");
+        recipeForm.reset();
+      }
+    });
+
+  }
+
+  submitButton.addEventListener('click', handleAddRecipes);
+
+
+
 }());
